b6dc71dd3943b832c78d5f56a1cde352493ee987
Eliene Sousa <eliene.nunes1@hotmail.com>
2022-03-22 00:43:17 -0300

    Cria Task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,16 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle){
+      return
+    }
+    const tarefa ={
+      id: Math.floor(Math.random()*10),
+      title:newTaskTitle,
+      isComplete: false
+    }
+    setTasks([...tasks,tarefa])
+    setNewTaskTitle("")
   }
 
   function handleToggleTaskCompletion(id: number) {
